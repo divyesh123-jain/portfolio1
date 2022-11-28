2a13332b3418c15ff2e614fd064d86dc3a5dd02a
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2022-11-28 23:51:24 +0530

    background added

--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import bg  from '../Images/bg 1.jpg'
+import Image from 'next/image';
 
 const Hero = () => {
   return (
     <div className=''>
-    <h1 className='flex justify-center text-white text-2xl align items-cente pt-32 '>Hey,</h1>
-     <p className='text-white flex justify-center pt-5 text-3xl'>THIS IS DIVYESH JAIN</p>
-     <p className='text-white flex justify-center pt-5 text-xl'>I'm A Frontend Developer</p>
+    <div className=''>
+    <Image className='z-[-1] 	position: fixed h-[100vh] w-[100vw]'
+      src={bg}
+      alt="background"
+      layout="fill"
+      objectFit='cover'
+       />
+
+</div>
+       <div>
+        <p className='flex justify-end mx-96 pt-36 text-2xl  text-white'>Hey,</p>
+        <p className='flex justify-end mx-52 pt-3  text-2xl  text-white'>I am Divyesh Jain</p>
+       </div>
     </div>
   );
 }
