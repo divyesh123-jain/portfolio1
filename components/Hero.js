abc43a06d92e3c2a654c538c35168f395e1ca99f
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2022-11-29 00:42:31 +0530

    hero added

--- components/Hero.js
+++ components/Hero.js
@@ -14,11 +14,25 @@ const Hero = () => {
        />
 
 </div>
-       <div>
-        <p className='flex justify-end mx-96 pt-36 text-2xl  text-white'>Hey,</p>
-        <p className='flex justify-end mx-52 pt-3  text-2xl  text-white'>I am Divyesh Jain</p>
-       </div>
+       <div className='mx-auto z-50 '>
+       <div className='text-white'>
+     <div className='max-w-[800px] mt-[-90px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold'>Hey,</p>
+        <h1 className='md:text-4xl sm:text-3xl text-4xl font-bold md:py-6'>This is Divyesh Jain</h1>
+        <div className='flex justify-center items-center'>
+            <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
+            I am a Frontend Developer
+            </p>
+            
+        </div>
+        <p className='md:text-2xl text-xl p-2  text-gray-400'>Hard Work Is Worthless For Those That Don't Believe In Themselves</p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3'>AboutMe</button>
+     </div>
     </div>
+       </div>
+       </div>
+    
+    
   );
 }
 
