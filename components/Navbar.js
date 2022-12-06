d7dc4963c58a1aca5bb0eafd6f72c7fd1fe163c0
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2022-12-06 16:56:55 +0530

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -162,7 +162,7 @@ const Navbar = () => {
               </p>
               <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                 <a
-                  href='https://www.linkedin.com/in/clint-briley-50056920a/'
+                  href='https://www.linkedin.com/in/divyesh-jain-145407231/'
                   target='_blank'
                   rel='noreferrer'
                 >
