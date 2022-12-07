7d0506fbc0dea8716d1f243162b5762e0f316277
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2022-12-07 19:14:06 +0530

    card added

--- pages/projects.js
+++ pages/projects.js
@@ -7,13 +7,6 @@ import { useMotionValue, useTransform, motion } from 'framer-motion';
 
 const Projects = () => {
  
-  const colors = [
-    { value: '#b6a179' },
-    { value: '#272425' },
-    { value: '#6389cb' },
-    { value: '#f2c758' },
-    { value: '#ffffff' },
-  ];
 
   return (<>
 
@@ -21,61 +14,53 @@ const Projects = () => {
   <div>
     <h1 className='text-white pt-28 align text-center text-3xl'>PROJECTS</h1>
   </div>
-  <div style={{ perspective: 2000 }}>
-      {/* card */}
-      <motion.div
-        
-        drag
-        dragElastic={0.18}
-        dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
-        whileTap={{ cursor: 'grabbing' }}
-        className='mx-14 w-[426px] min-h-[600px] bg-black  rounded-[30px] border-[4px] border-white px-[40px] py-[24px] cursor-grab relative'
+  <div className='mx-4 pt-12 grid  grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-8'>
+  <div className='w-[426px] min-h-[600px] bg-[#e4dfdc] rounded-[30px] border-[4px] border-white px-[40px] py-[24px] cursor-grab relative'
       >
-         <motion.div
-        
-          className='absolute top-1 -right-64 w-[620px]'
-        >
-          <Image src={hcare} alt=''
-           className='w-[40vh]  relative' draggable='false' />
-            
-        </motion.div>
+        {/* card logo */}
+        <div className='mb-6'>
+          <img src={hcare} alt='' />
+
+        </div>
         {/* card title */}
-        <h1 className='text-5xl mb-6 font-extrabold pt-28 text-gray-400'>HEATHCARE DASHBOARD</h1>
+        <h1 className='text-5xl mb-6 font-extrabold'>HEALTHCARE DASHBOARD</h1>
         {/* card subtitle */}
-        <p className='max-w-[300px] text-gray-400 mb-6'>
-          a  healthcare dashboard that caters to your healthcare needs 
+        <p className='max-w-[300px] text-[#000000] mb-6'>
+         its is a healthcare dashboard that caters to your healthcare needs
         </p>
         {/* btn & price wrapper */}
         <div className='flex items-center gap-x-[20px] mb-12'>
           <button className='bg-[#2d2b2c] text-white text-base font-medium py-[16px] px-[40px] rounded-lg'>
-            View  
+            View
           </button>
           <div className='text-[24px] font-bold text-[#000000]'></div>
+          </div>
+          </div>
+
+          <div className='w-[426px] min-h-[600px] bg-[#e4dfdc] rounded-[30px] border-[4px] border-white px-[40px] py-[24px] cursor-grab relative'
+      >
+        {/* card logo */}
+        <div className='mb-6'>
+          <img src={hcare} alt='' />
+
         </div>
-        {/* colors */}
-        <ul className='flex gap-x-[10px]'>
-          {colors.map((color, index) => {
-            return (
-              <li
-                key={index}
-                style={{ backgroundColor: color.value }}
-                className='w-8 h-8 rounded-full cursor-pointer'
-              ></li>
-            );
-          })}
-        </ul>
-        {/* card image */}
-      
-      </motion.div>
-    </div>
+        {/* card title */}
+        <h1 className='text-5xl mb-6 font-extrabold'>HEALTHCARE DASHBOARD</h1>
+        {/* card subtitle */}
+        <p className='max-w-[300px] text-[#000000] mb-6'>
+         its is a healthcare dashboard that caters to your healthcare needs
+        </p>
+        {/* btn & price wrapper */}
+        <div className='flex items-center gap-x-[20px] mb-12'>
+          <button className='bg-[#2d2b2c] text-white text-base font-medium py-[16px] px-[40px] rounded-lg'>
+            View
+          </button>
+          <div className='text-[24px] font-bold text-[#000000]'></div>
+          </div>
+          </div>
 
 
-  
-
-
-
-
-
+       </div>
     </>
   );
 }
